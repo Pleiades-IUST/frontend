c5b978461abd887046fad1dacdbeedf80ac6938b
HamidReza Montazer <montazerh82@gmail.com>
2025-06-05 19:43:40 +0330

    chore

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 
-import { Flex } from '@chakra-ui/react';
+import { Flex, Tabs } from '@chakra-ui/react';
 
 import MapComponent from '@/components/MapComponent';
 import Sessions from '@/components/Sessions';
 import Filters from '@/components/Filters';
 import { DriveTestData } from '@/mock/Data';
 import { sessionsData } from '@/mock/Sessions';
+import DataTable from '@/components/DataTable';
 
 function Dashboard() {
   const [selectedSessionId, setSelectedSessionId] = useState(null);
@@ -16,10 +17,10 @@ function Dashboard() {
   const [valueKey, setValueKey] = useState('RSRP');
 
   const [filters, setFilters] = useState([
-    { id: 'rsrp', label: 'Show Roads', checked: true },
-    { id: 'buildings', label: 'Show Buildings', checked: false },
-    { id: 'water', label: 'Show Water', checked: false },
-    { id: 'parks', label: 'Show Parks', checked: false },
+    { id: 'rsrp-rscp', label: 'Show RSRP/RSCP', checked: true },
+    { id: 'rsrq-ecn0', label: 'Show RSRQ/Ec-N0', checked: false },
+    { id: 'cellid', label: 'Show Cell ID', checked: false },
+    { id: 'lac-tac', label: 'Show LAC/TAC', checked: false },
   ]);
   const handleFilterChange = (selectedId) => {
     setFilters((prev) =>
@@ -37,8 +38,26 @@ function Dashboard() {
         selectedSessionId={selectedSessionId}
         setSession={setSelectedSessionId}
       />
-      <MapComponent points={points} discrete={discrete} valueKey={valueKey} />
-      <Filters filters={filters} onFilterChange={handleFilterChange} />
+
+      <Tabs.Root defaultValue="map">
+        <Tabs.List h={'6vh'}>
+          <Tabs.Trigger value="map">Map</Tabs.Trigger>
+          <Tabs.Trigger value="table">Table</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="map">
+          <Flex>
+            <MapComponent
+              points={points}
+              discrete={discrete}
+              valueKey={valueKey}
+            />
+            <Filters filters={filters} onFilterChange={handleFilterChange} />
+          </Flex>
+        </Tabs.Content>
+        <Tabs.Content value="table">
+          <DataTable data={DriveTestData} />
+        </Tabs.Content>
+      </Tabs.Root>
     </Flex>
   );
 }
