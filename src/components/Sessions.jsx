198ecd35ae3d165d2b19fa4b9c8baf8a441d9371
HamidReza Montazer <montazerh82@gmail.com>
2025-06-05 19:01:07 +0330

    feat: adding sessions sidebar

---
+++ src/components/Sessions.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Sessions.css';
+
+const Sessions = ({ sessions, selectedSessionId, setSession }) => {
+  return (
+    <aside className="sidebar">
+      <h2 className="sidebar-title">Sessions</h2>
+      <ul className="session-list">
+        {sessions.map((session) => (
+          <li
+            key={session.id}
+            className={`session-item ${
+              selectedSessionId === session.id ? 'active' : ''
+            }`}
+            onClick={() => setSession(session.id)}
+          >
+            {session.name}
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sessions;
